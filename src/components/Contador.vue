4db0d90a0d24939776337c7338c18c5b4caad821
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2022-07-07 19:42:30 -0500

    taller 14

---
+++ src/components/Contador.vue
@@ -0,0 +1,65 @@
+<template>
+    <!-- Html-->
+    <h2>Contador!</h2>
+    <p>{{ numero }} <sup>2</sup> = {{ obtenerCuadradoComputado }}</p>
+    <p>{{ numero }} <sup>2</sup> = {{ obtenerCuadradoComputado }}</p>
+    <p>{{ numero }} <sup>2</sup> = {{ obtenerCuadradoComputado }}</p>
+    <p>{{ numero }} <sup>2</sup> = {{ obtenerCuadradoComputado }}</p>
+    <p>{{ numero }} <sup>2</sup> = {{ obtenerCuadradoComputado }}</p>
+
+    <div>
+        <button v-on:click="incrementar">
+            +1
+        </button>
+
+        <button v-on:click="decrementar">
+            -1
+        </button>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: "Contador123",
+    data() {
+        return {
+            numero: 5
+        }
+    },
+    methods: {
+        obtenerCuadrado() {
+            console.log("Entrando a la potencia")
+            return this.numero * this.numero
+        },
+        incrementar(){
+            return this.numero++
+        },
+        decrementar(){
+            return this.numero--
+        }
+    },
+    computed:{
+        obtenerCuadradoComputado(){
+            console.log("Entrando a la computado")
+            return this.numero * this.numero
+        }
+    }
+}
+</script>
+<!-- Estilos de mi componentes -->
+<style>
+button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    color: white;
+    margin: 0 5px;
+    cursor: pointer;    
+}
+
+button:hover{
+    background-color: #5AA67B;
+    transition: 0.1s ease-in-out;
+}
+</style>
