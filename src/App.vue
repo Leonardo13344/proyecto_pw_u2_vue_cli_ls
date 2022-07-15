d74adc6e7cacbe0d4185fd58e78ccfcfdce2c4d0
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2022-07-14 19:43:04 -0500

    taller16

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <!--<HelloWorld msg="Bienvenido to Your Vue.js App"/>-->
-  <!--<Contador2 titulo="Titulo1"  v-bind:inicio = "valorInicio" />-->
-  <!--<Contador2 titulo="Titulo3" v-bind:inicio="-1" />-->
-  <!--<Calculadora />-->
-  <DatosBasicos v-bind:tipo="tipo"/>
+  <Pregunta />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Contador from './components/Contador.vue'
-import Contador2 from './components/Contador2.vue'
-import Calculadora from './components/Calculadora.vue'
-import DatosBasicos from './components/DatosBasicos.vue'
 
+import Pregunta from './components/Pregunta.vue'
 export default {
   name: 'App',
   data(){
     return{
-      valorInicio: 27,
-      tipo: "Estudiante"
+      
     }
   },
   components: {
-    Contador,
-    Calculadora,
-    Contador2,
-    DatosBasicos
+    Pregunta,  
   }
 }
 </script>
